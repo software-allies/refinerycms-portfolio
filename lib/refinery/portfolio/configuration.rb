931cbba8e5d85f8d7b01e4138903b0b95eca524d
Christian <preth00nker@gmail.com>
2014-02-04 15:32:24 -0600

    adding description capability

--- lib/refinery/portfolio/configuration.rb
+++ lib/refinery/portfolio/configuration.rb
@@ -3,7 +3,7 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :items_per_page, :cover_image_thumb_size, :skitter_options,
-                    :display_item_text, :display_gallery_text
+                    :display_item_text, :display_gallery_text, :description
 
     self.items_per_page = 20
     self.cover_image_thumb_size = '300x200'
@@ -21,6 +21,7 @@ module Refinery
       # :structure => ""            # Use this to override the HTML if utterly necessary.
     }
 
+    self.description = "Some description"
     self.display_item_text = true
     self.display_gallery_text = true
   end
